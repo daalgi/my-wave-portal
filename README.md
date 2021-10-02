3296c62adaf8ae2c3ddce77cf05d39b49cc5ef5f
daalgi <daalgicam@gmail.com>
2021-10-02 16:33:49 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,22 @@
+# Basic Sample Hardhat Project
+
+This project demonstrates a basic Hardhat use case. It comes with a sample contract, a test for that contract, a sample script that deploys that contract, and an example of a task implementation, which simply lists the available accounts.
+
+Try running some of the following tasks:
+
+```shell
+npx hardhat accounts
+npx hardhat compile
+npx hardhat clean
+npx hardhat test
+npx hardhat node
+node scripts/sample-script.js
+npx hardhat help
+```
+
+Rinkeby testnet contract address:
+```0x382d4d13b4633333188C0c291877c67ABD0139FB```
+https://rinkeby.etherscan.io/address/0x382d4d13b4633333188C0c291877c67ABD0139FB
+
+Replit project
+https://replit.com/@DavidAG2/waveportal-baseline-student#README.md
---
+++ contracts/WavePortal.sol
@@ -0,0 +1,38 @@
+//SPDX-License-Identifier: Unlicense
+pragma solidity ^0.8.0;
+
+import "hardhat/console.sol";
+
+contract WavePortal {
+    uint256 totalWaves;
+
+    event NewWave(address indexed from, uint256 timestamp, string message);
+
+    struct Wave {
+        address waver;
+        string message;
+        uint256 timestamp;
+    }
+
+    Wave[] waves;
+
+    constructor() {
+        console.log("Constructing!");
+    }
+
+    function wave(string memory _message) public {
+        totalWaves += 1;
+        waves.push(Wave(msg.sender, _message, block.timestamp));
+
+        emit NewWave(msg.sender, block.timestamp, _message);
+    }
+
+    function getTotalWaves() public view returns (uint256) {
+        console.log("We have %d total waves!", totalWaves);
+        return totalWaves;
+    }
+
+    function getAllWaves() public view returns (Wave[] memory) {
+        return waves;
+    }
+}
